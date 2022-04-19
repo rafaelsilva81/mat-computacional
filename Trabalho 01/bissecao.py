f936d571824804eda43382c4478728e7a1e0de84
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2022-04-19 17:00:02 -0300

    Commit Inicial

---
+++ Trabalho 01/bissecao.py
@@ -0,0 +1,47 @@
+import os
+import numpy
+
+def isNegative( num ):
+    if num<0:
+        return True
+    else:
+        return False
+
+    
+def bissecao(func, a, b, precision):
+    data = {}
+    if (func(a) * func(b) >= 0):
+        return "Não há raizes nesse intervalo"
+
+    meio = a
+    while ((b - a) >= precision):
+        
+        meio = (a + b) / 2
+        printData(func, a, b, meio)
+        
+        if(func(meio) == 0):
+            return func(meio)
+    
+        if(isNegative(func(meio)) and isNegative(func(a))):
+            a = meio
+        else:
+            b = meio
+        
+        #printData(func, a, b, meio)
+    
+    return meio
+
+def main():
+    print("Exemplo de função --> 2*(x*x*x) - 5*(x*x) + x + 10")
+    i = input("Entre com a função no formato do terminal: ")  # enter function once
+    i = compile(i, 'input', 'eval')
+    a = int(input("Entre o primeiro intervalo : "))
+    b = int(input("Entre o segundo intervalo : "))
+    prec = float(input("Entre com a precisão : "))
+    func = lambda x : eval(i, {'x': x, 'np': numpy})
+
+    res = bissecao(func, a, b, prec)
+    print("Raiz encontrada : ", res)
+
+if __name__ == "__main__":
+    main()    
